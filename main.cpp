de5f2f16b72d6bb2fd7ce836fe4e515c0d7328f2
Даня Степанюк <stepanukd03@gmail.com>
2026-09-18 15:41:22 +0300

    stepanuik

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 10;
+    
+    int result = add(x, y);
+    
+    std::cout << "result: " << result << std::endl;
+
+    return 0;
+}
